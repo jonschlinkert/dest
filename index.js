a469db3d8e2045a7c91772d76e5d97ab61ac7320
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-14 14:47:08 -0400

    update to allow `file` to have a dest function

--- index.js
+++ index.js
@@ -1,101 +1,41 @@
 'use strict';
 
 var fs = require('fs');
+var gfs = require('graceful-fs');
 var path = require('path');
 var extend = require('extend-shallow');
+var through = require('through2');
 var mkdirp = require('mkdirp');
-var stripBom = require('strip-bom');
-var through2 = require('through2');
+
 
 module.exports = function dest(dir, options) {
-  var stream = through2.obj(function (file, enc, cb) {
-    normalize(dir, file, options, function (err, fp) {
+  var stream = through.obj(function (file, enc, cb) {
+    var opts = normalizeOpts(file, options);
+    if (typeof file.dest === 'function') {
+      return file.dest(dir, opts, function (err, fp) {
+        if (err) return cb(err);
+        writeFile(fp, file, opts, cb);
+      });
+    }
+
+    normalize(dir, file, opts, function (err, fp) {
       if (err) return cb(err);
-      writeContents(fp, file, cb);
+      writeFile(fp, file, opts, cb);
     });
   });
   stream.resume();
   return stream;
 };
 
-function streamFile(file, cb) {
-  file.contents = fs.createReadStream(file.path).pipe(stripBom.stream());
-  cb(null, file);
+function writeFile(fp, file, opts, cb) {
+  // mkdirp the folder the file is going in
+  mkdirp(path.dirname(fp), opts.dirMode, function (err) {
+    if (err) return cb(err);
+    writeContents(fp, file, cb);
+  });
 }
 
-function writeContents(destDir, file, cb) {
-  if (file.isDirectory()) {
-    return writeDir(destDir, file, written);
-  }
-  if (file.isStream()) {
-    return writeStream(destDir, file, written);
-  }
-  if (file.isBuffer()) {
-    return writeBuffer(destDir, file, written);
-  }
-  if (file.isNull()) {
-    return done();
-  }
-  function done(err) {
-    cb(err, file);
-  }
-  function written(err) {
-    if (isErrorFatal(err)) return done(err);
-    if (!file.stat || typeof file.stat.mode !== 'number') {
-      return done();
-    }
-    fs.stat(destDir, function (err, st) {
-      if (err) return done(err);
-      // octal 7777 = decimal 4095
-      var currentMode = (st.mode & 4095);
-      if (currentMode === file.stat.mode) {
-        return done();
-      }
-      fs.chmod(destDir, file.stat.mode, done);
-    });
-  }
-
-  function isErrorFatal(err) {
-    if (!err) return false;
-    if (err.code === 'EEXIST' && file.flag === 'wx') {
-      return false;
-    }
-    return true;
-  }
-}
-
-function writeBuffer(destDir, file, cb) {
-  var opt = { mode: file.stat.mode, flag: file.flag };
-  fs.writeFile(destDir, file.contents, opt, cb);
-}
-
-function writeDir(destDir, file, cb) {
-  mkdirp(destDir, file.stat.mode, cb);
-}
-
-function writeStream(destDir, file, cb) {
-  var opt = { mode: file.stat.mode, flag: file.flag };
-
-  var outStream = fs.createWriteStream(destDir, opt);
-  file.contents.once('error', complete);
-  outStream.once('error', complete);
-  outStream.once('finish', success);
-
-  file.contents.pipe(outStream);
-
-  function success() {
-    streamFile(file, complete);
-  }
-
-  function complete(err) {
-    file.contents.removeListener('error', cb);
-    outStream.removeListener('error', cb);
-    outStream.removeListener('finish', success);
-    cb(err);
-  }
-}
-
-function normalize(dest, file, options, cb) {
+function normalizeOpts(file, options) {
   var opts = extend({
     cwd: process.cwd(),
     mode: (file.stat ? file.stat.mode : null),
@@ -104,6 +44,116 @@ function normalize(dest, file, options, cb) {
   }, options);
 
   opts.flag = (opts.overwrite ? 'w' : 'wx');
+  return opts;
+}
+
+function streamFile(file, opts, cb) {
+  file.contents = fs.createReadStream(file.path);
+  cb(null, file);
+}
+
+function writeContents(writePath, file, cb) {
+  if (file.isDirectory()) {
+    return writeDir(writePath, file, written);
+  }
+  if (file.isStream()) {
+    return writeStream(writePath, file, written);
+  }
+  if (file.isBuffer()) {
+    return writeBuffer(writePath, file, written);
+  }
+  if (file.symlink) {
+    return writeSymbolicLink(writePath, file, written);
+  }
+  if (file.isNull()) {
+    return complete();
+  }
+
+  function complete(err) {
+    cb(err, file);
+  }
+
+  function written(err) {
+    if (isErrorFatal(err)) {
+      return complete(err);
+    }
+
+    if (!file.stat || typeof file.stat.mode !== 'number' || file.symlink) {
+      return complete();
+    }
+
+    fs.stat(writePath, function(err, st) {
+      if (err) {
+        return complete(err);
+      }
+      var currentMode = (st.mode & parseInt('0777', 8));
+      var expectedMode = (file.stat.mode & parseInt('0777', 8));
+      if (currentMode === expectedMode) {
+        return complete();
+      }
+      fs.chmod(writePath, expectedMode, complete);
+    });
+  }
+
+  function isErrorFatal(err) {
+    if (!err) {
+      return false;
+    }
+
+    // Handle scenario for file overwrite failures.
+    else if (err.code === 'EEXIST' && file.flag === 'wx') {
+      return false;
+    }
+
+    // Otherwise, this is a fatal error
+    return true;
+  }
+}
+
+function writeBuffer(destDir, file, cb) {
+  var opt = { mode: file.stat.mode, flag: file.flag };
+  gfs.writeFile(destDir, file.contents, opt, cb);
+}
+
+function writeDir(destDir, file, cb) {
+  mkdirp(destDir, file.stat.mode, cb);
+}
+
+function writeStream(destDir, file, cb) {
+  var opts = {mode: file.stat.mode, flag: file.flag};
+
+  var outStream = gfs.createWriteStream(destDir, opts);
+
+  file.contents.once('error', complete);
+  outStream.once('error', complete);
+  outStream.once('finish', success);
+
+  file.contents.pipe(outStream);
+
+  function success() {
+    streamFile(file, {}, complete);
+  }
+
+  // cleanup
+  function complete(err) {
+    file.contents.removeListener('error', cb);
+    outStream.removeListener('error', cb);
+    outStream.removeListener('finish', success);
+    cb(err);
+  }
+}
+
+function writeSymbolicLink(destDir, file, cb) {
+  gfs.symlink(file.symlink, destDir, function (err) {
+    if (err && err.code !== 'EEXIST') {
+      return cb(err);
+    }
+    cb(null, file);
+  });
+}
+
+function normalize(dest, file, opts, cb) {
+  opts = opts || {};
   var cwd = path.resolve(opts.cwd);
 
   if (typeof dest !== 'string' && typeof dest !== 'function') {
@@ -111,21 +161,30 @@ function normalize(dest, file, options, cb) {
   }
 
   var destPath = (typeof dest === 'string' ? dest : dest(file));
-  var basePath = path.resolve(cwd, destPath);
+
+  var base = opts.base;
+  if (base && typeof base !== 'string' && typeof base !== 'function') {
+    throw new Error('Invalid base option');
+  }
+
+  var basePath = base
+    ? (typeof base === 'string' ? base : base(file))
+    : path.resolve(cwd, destPath);
+
+  if (typeof basePath !== 'string') {
+    throw new Error('Invalid base option');
+  }
+
   var filepath = path.resolve(basePath, path.basename(file.relative));
   var dir = path.dirname(filepath);
 
   // wire up new properties
-  file.stat = (file.stat || new fs.Stats());
+  file.stat = (file.stat || new gfs.Stats());
   file.stat.mode = opts.mode;
   file.flag = opts.flag;
   file.cwd = cwd;
   file.base = basePath;
   file.path = filepath;
 
-  // mkdirp the folder the file is going in
-  mkdirp(dir, opts.dirMode, function (err) {
-    if (err) return cb(err);
-    cb(null, filepath);
-  });
+  cb(null, filepath);
 }
